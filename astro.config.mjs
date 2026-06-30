0054ca5081eebe7801c5e90dafffb9eee4a94a00
Ashfaqur Rahman <your-email@example.com>
2026-07-01 04:01:11 +0600

    fix: set astro base path for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
 
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  // Ensure this is set to '/' for Cloudflare Pages
+  base: '/', 
 });
